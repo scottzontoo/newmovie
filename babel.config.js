62f43a16079c650e998d99747a0ec6091b248787
scottzontoo <scottlarryzonto@gmail.com>
2025-08-10 06:43:55 +0000

    Add NativeWind preset to Babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+  // NativeWind provides a Babel preset (not a plugin), so include it in presets
+  presets: ["babel-preset-expo", "nativewind/babel"],
   };
 };
